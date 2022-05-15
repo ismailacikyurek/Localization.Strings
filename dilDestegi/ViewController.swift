5cf0f96e84ee739c276213b405867913860d2d24
ismailacikyurek <ismailackyurek@gmail.com>
2022-05-15 13:28:47 +0300

    first commit

---
+++ dilDestegi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dilDestegi
+//
+//  Created by İSMAİL AÇIKYÜREK on 15.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
